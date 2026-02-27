cb0e861fef917515695ae0be6d7e3d3d7f564e7d
Gillian Smith <Gillian@example.com>
2026-02-27 22:12:44 +0800

    fix: Remove router entirely, render Home directly for GitHub Pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,3 @@
-import { Switch, Route, Router } from "wouter";
-import { useHashLocation } from "wouter/use-hash-location";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -11,11 +9,7 @@ function App() {
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
         <Toaster />
-        <Router hook={useHashLocation}>
-          <Switch>
-            <Route path="/" component={Home} />
-          </Switch>
-        </Router>
+        <Home />
       </TooltipProvider>
     </QueryClientProvider>
   );
